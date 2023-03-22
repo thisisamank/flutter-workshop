adce7152b2042cb6a081f507f95b14cbd607dd3c
Aman Kumar <thisisamank@gmail.com>
2023-03-23 05:14:04 +0530

    feat: meta and structure

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://flutter-workshop.thisisamank.in",
+  url: "https://flutter-workshop.thisisamank.in/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
